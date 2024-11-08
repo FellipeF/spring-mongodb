9fd1c89d49bf025163166464c5195aa5d06a8c31
FellipeF <fellipefreire99@hotmail.com>
2024-11-07 23:07:56 -0300

    DTO pattern implementation for Users

--- src/main/java/com/java/springmongo/resources/UserResource.java
+++ src/main/java/com/java/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.java.springmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.springmongo.domain.User;
+import com.java.springmongo.dto.UserDTO;
 import com.java.springmongo.services.UserService;
 
 @RestController
@@ -19,10 +21,11 @@ public class UserResource {
 	private UserService service;	//REST Controller has access to UserService, that has access to the Repository
 	
 	@RequestMapping(method = RequestMethod.GET)	//Alternative to GetMapping
-	public ResponseEntity<List<User>> findAll()
+	public ResponseEntity<List<UserDTO>> findAll()
 	{	
 		List <User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List <UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
